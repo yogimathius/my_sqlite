3b4a17a65d2150b6dd76a3cc2fc2582bcaf299a1
rebecca silverstein <silverst_r@qwasar.io>
2024-01-03 23:06:24 +0000

    chore: use regex to make parsing of query case-insensitive

--- my_sqlite_cli.rb
+++ my_sqlite_cli.rb
@@ -14,16 +14,16 @@ class MySqliteQueryCli
     end
 
     def build_join(string)
-        join_table, join_clause = string.split(" ON ")
+        join_table, join_clause = string.split(/ ON /i)
         join_id_a, join_id_b = join_clause.split("=").map(&:strip)
         @request.join(join_table, join_id_a, join_id_b)
     end
 
     def build_select(string)
-        remaining_clause, where_clause = string.split("SELECT ")[1].split(" WHERE ")
+        remaining_clause, where_clause = string.split(/SELECT /i)[1].split(/ WHERE /i)
 
-        select_clause, from_clause = remaining_clause.split("FROM ")
-        from_table, join_clause = from_clause.split(" JOIN ")
+        select_clause, from_clause = remaining_clause.split(/FROM /i)
+        from_table, join_clause = from_clause.split(/ JOIN /i)
         select_columns = select_clause.split(/[,\s]+/)
 
         build_join(join_clause) unless join_clause.nil?
@@ -35,17 +35,17 @@ class MySqliteQueryCli
     end
 
     def build_insert(string)
-        insert_clause, remaining_clause = string.split(" VALUES ")
-        insert_into = insert_clause.split("INTO ")[1]
+        insert_clause, remaining_clause = string.split(/ VALUES /i)
+        insert_into = insert_clause.split(/INTO /i)[1]
         values = remaining_clause.split(",").map(&:strip)
         @request.insert(insert_into)
                 .values(values)
     end
 
     def build_update(string)
-        update_clause, remaining_clause = string.split(" SET ")
+        update_clause, remaining_clause = string.split(/ SET /i)
         update_from = update_clause.split(" ")[1]
-        set_clause, where_clause = remaining_clause.split(" WHERE ")
+        set_clause, where_clause = remaining_clause.split(/ WHERE /i)
         set_hash = set_clause.split(",").map do |part|
             key, value = part.split('=').map(&:strip)
             [key, value]
@@ -58,11 +58,11 @@ class MySqliteQueryCli
     end
 
     def build_delete(string)
-        delete_from, where_clause = string.split("FROM ")[1].split(" WHERE ")
+        delete_from, where_clause = string.split(/FROM /i)[1].split(/ WHERE /i)
         
         build_where(where_clause) unless where_clause.nil?
 
-        @request.delete()
+        @request.delete
                 .from(delete_from)
     end
 
@@ -70,13 +70,13 @@ class MySqliteQueryCli
         modified_buf = buf.delete("();'") # remove punctuation
         p modified_buf
 
-        if modified_buf.include?("SELECT")
+        if modified_buf.match?(/SELECT/i)
             result = build_select(modified_buf)
-        elsif modified_buf.include?("INSERT")
+        elsif modified_buf.match?(/INSERT/i)
             result = build_insert(modified_buf)
-        elsif modified_buf.include?("UPDATE")
+        elsif modified_buf.match?(/UPDATE/i)
             result = build_update(modified_buf)
-        elsif modified_buf.include?("DELETE")
+        elsif modified_buf.match?(/DELETE/i)
             result = build_delete(modified_buf)
         end
         
@@ -94,9 +94,9 @@ class MySqliteQueryCli
 end
 
 
-def _main()     # main must be left uncommented to run CLI in docode
-    mysqcli = MySqliteQueryCli.new
-    mysqcli.run!
-end
+# def _main()     # main must be left uncommented to run CLI in docode
+#     mysqcli = MySqliteQueryCli.new
+#     mysqcli.run!
+# end
     
-_main()
+# _main()
