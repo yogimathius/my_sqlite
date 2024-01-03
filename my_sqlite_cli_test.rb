3587132e0786f4c82291b51dfabc503c7d00a256
rebecca silverstein <silverst_r@qwasar.io>
2024-01-03 03:00:26 +0000

    chore: added test_build_select_with_join

--- my_sqlite_cli_test.rb
+++ my_sqlite_cli_test.rb
@@ -5,15 +5,25 @@ class TestMySqliteQueryCli < Test::Unit::TestCase
   def setup
     @cli = MySqliteQueryCli.new
     File.write('test_data.csv', "id,name,age\n1,John Doe,25\n2,Jane,30\n3,Bob,22\n")
+    File.write('joins_data.csv', "owner_id,dog_name\n1,Fido\n2,Windy\n3,Chickpea\n")
   end
 
   def test_build_select
-    query = "SELECT name, age FROM test_data.csv WHERE name = 'John Doe'"
+    query = "SELECT name, age FROM test_data.csv WHERE name = 'John Doe'"    result = @cli.parse(query)
+    assert_equal(:select, result.instance_variable_get(:@type_of_request))
+    assert_equal(["name", "age"], result.instance_variable_get(:@select_columns))
+    assert_equal("test_data.csv", result.instance_variable_get(:@table_name))
+    assert_equal([["name", "John Doe"]], result.instance_variable_get(:@where_params))
+  end
+
+  def test_build_select_with_join
+    query = "SELECT name, age, dog_name FROM test_data.csv JOIN joins_data.csv ON id = owner_id WHERE name = 'John Doe'"
     result = @cli.parse(query)
     assert_equal(:select, result.instance_variable_get(:@type_of_request))
     assert_equal(["name", "age"], result.instance_variable_get(:@select_columns))
     assert_equal("test_data.csv", result.instance_variable_get(:@table_name))
     assert_equal([["name", "John Doe"]], result.instance_variable_get(:@where_params))
+    assert_equal({filename_db_b: "joins_data.csv", column_on_db_a: "id", column_on_db_b: "owner_id"}, result.instance_variable_get(:@join_attributes))
   end
 
   def test_build_update
@@ -43,6 +53,6 @@ class TestMySqliteQueryCli < Test::Unit::TestCase
 
   def teardown
     File.delete('test_data.csv') if File.exist?('test_data.csv')
-    # File.delete('joins_data.csv') if File.exist?('joins_data.csv')
+    File.delete('joins_data.csv') if File.exist?('joins_data.csv')
   end
 end
