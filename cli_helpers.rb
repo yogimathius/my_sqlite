965a62a09d4e49bbbe6cd25ff4c38862a3f95d50
mathius johnson <johnson_m@qwasar.io>
2024-01-06 08:24:17 +0000

    chore: cleanup cli_helpers parse_select

--- cli_helpers.rb
+++ cli_helpers.rb
@@ -15,11 +15,8 @@ class CliHelpers
         delimiters = ['SELECT ', ' FROM ', ' JOIN ', ' ON ', ' WHERE ', ' ORDER BY']
 
         result = {}
-        delimiters.each_with_index do |delimiter, index|
+        delimiters.each do |delimiter|
             parts = string.split(delimiter)[1] if string.include?(delimiter)
-            if !(string.empty? or parts.nil? or !string.include?(delimiters[index + 1])) && (string and string.include?(delimiters[index + 1]))
-              parts = parts.split(delimiters[index + 1])
-            end
             parts = parts.first if parts.kind_of?(Array)
             result[delimiter.strip.to_sym] = slice_delims(parts, delimiters)
         end
